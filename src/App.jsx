6ce9efdbb2fe7c59c497de43fd26fb096e6dc3f5
Opie Taylor <taborn.nequil@gmail.com>
2024-07-11 15:18:56 -0500

    week 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [
+  {
+    id: 123, 
+    title: "Practice", 
+  }, 
+  {
+    id: 234, 
+    title: "Read", 
+  }, 
+  {
+    id: 345, 
+    title: "Project", 
+  }
+]; 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
-  const todoList = [
-    {
-      id: 123, 
-      title: "Practice", 
-    }, 
-    {
-      id: 234, 
-      title: "Read", 
-    }, 
-    {
-      id: 345, 
-      title: "Project", 
-    }
-  ]; 
-
+  
   return (
-    <>
-      <h1>Todo List</h1>
-      <ul>{todoList.map(function (item){
-        return (
-          <li>
-          <span>{item.id}</span>
-          <br/>
-          <span>{item.title}</span>
-          </li> 
-        ); 
-      })}</ul>
-    </>
-  )
-}
 
-export default App
+    <>  
+      <h1>Todo List</h1>
+      <ul>
+      {todoList.map(({ id, title}) => (
+        <li key={id}>
+          <span>{id} </span>
+          <br/> 
+          <span>{title}</span>
+      </li>
+    ))}
+    </ul> 
+  
+    </>
+)}
+export default App;
