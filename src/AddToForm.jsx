5909e37db801a81f4735e56e20dfc19f85ede88f
Opie Taylor <taborn.nequil@gmail.com>
2024-07-13 20:06:49 -0500

    week2

---
+++ src/AddToForm.jsx
@@ -0,0 +1,17 @@
+
+
+const AddTodoForm = () => {
+
+    return(
+        
+        <form>
+            <label htmlFor="todoTitle"></label>
+            <input type="text" id="todoTitle"/> 
+            <button type="submit">Add</button>
+        </form>
+        
+    )
+
+}
+
+export default AddTodoForm
