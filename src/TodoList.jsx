a2b9e4c4099aaf87e147327accbaad238bcb4e62
Opie Taylor <taborn.nequil@gmail.com>
2024-12-11 16:13:42 -0600

    changes

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
 const TodoList = ({ todos }) => {
   return (
     <ul>
-      {todos.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
+      {todos.map((todo) => ( // map method iterates over the "todos" array
+        <TodoListItem key={todo.id} todo={todo} /> //TodoListItem component
       ))}
     </ul>
   );
